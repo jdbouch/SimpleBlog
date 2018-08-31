8de9163ccf1cca9ad8640216f157d82bd60bf13c
James Bouchard <jdbouch3144@live.com>
2018-08-31 10:27:08 -0400

    Route updates initially

--- SimpleBlog/App_Start/RouteConfig.cs
+++ SimpleBlog/App_Start/RouteConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using SimpleBlog.Controllers;
 
 namespace SimpleBlog
 {
@@ -11,12 +12,19 @@ namespace SimpleBlog
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            var namespaces = new[] { typeof(PostsController).Namespace};
+
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapRoute("Login",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Auth", action = "Login", id = "namespaces" }
+
+            );
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Posts", action = "Index", id = "namespaces" }
             );
         }
     }
